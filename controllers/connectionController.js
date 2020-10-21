2e5395356c79bf0107099c0eaa23810352d23bea
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-10-21 17:16:28 +0100

    query created

--- controllers/connectionController.js
+++ controllers/connectionController.js
@@ -8,7 +8,8 @@ exports.getIndex = async (req, res) => {
 }
 
 exports.getStaffMember = async (req, res) => {
-    let values = await functions.SelectStaffMember();
+    let {ID} = req.query;
+    let values = await functions.SelectStaffMember(ID);
     let display = values.recordsets;
     console.log(display);
     res.send(display);
